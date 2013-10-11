7dbd2e00f8bc6681274a5b587bdc3559df487c39
David Becerra <davidbecerra84@gmail.com>
2013-10-11 01:03:42 -0700

    configure testing environment - it's hawt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,25 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+#require 'spork/ext/ruby-debug'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
-  config.include Capybara::DSL
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+    config.include Capybara::DSL
+  end
+end
+
+Spork.each_run do
 end
