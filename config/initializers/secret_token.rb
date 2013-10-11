2b177add8dd468f7a0675338e01490cc7beda499
David Becerra <davidbecerra84@gmail.com>
2013-10-10 19:27:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkstack::Application.config.secret_key_base = 'de17ea97380d8d8fb0b72c351aceaf67a9a02b73c8d5e63d61f5912a717b54a96c002c32ae0f9784f745ce5af03da7b3c56f90b9716767072ffffbdd5c6c72ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkstack::Application.config.session_store :cookie_store, key: '_linkstack_session'
