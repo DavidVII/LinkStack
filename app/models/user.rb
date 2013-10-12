dd79f0acaaf8749b9d69cfd1f5bf21831c1ce43b
David Becerra <davidbecerra84@gmail.com>
2013-10-12 03:06:49 -0700

    add username to user and start user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9_]*[a-zA-Z][a-zA-Z0-9_]*\z/
+  validates :username, presence: true, length: { minimum: 2, maximum: 50 }, format: { with: VALID_USERNAME_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(
+      username: 'ExampleUser',
+      email: "user@example.com",
+      password: "foobar1",
+      password_confirmation: "foobar1"
+    )
+  end
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:encrypted_password) }
+  it { should respond_to(:reset_password_token) }
+  it { should respond_to(:reset_password_sent_at) }
+  it { should respond_to(:remember_created_at) }
+  it { should respond_to(:sign_in_count) }
+  it { should respond_to(:current_sign_in_at) }
+  it { should respond_to(:last_sign_in_at) }
+  it { should respond_to(:current_sign_in_ip) }
+  it { should respond_to(:last_sign_in_ip) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.username = " " }
+    it { should_not be_valid }
+  end
 end
