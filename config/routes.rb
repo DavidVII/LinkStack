3c8a90bde310b0de8f58743c6d3d99522ea17124
David Becerra <davidbecerra84@gmail.com>
2013-10-11 23:09:22 -0700

    add full_title helper and the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Linkstack::Application.routes.draw do
+  match 'about', to: 'pages#about', via: :get
+
   root 'pages#index'
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -6,9 +6,18 @@ describe "Pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_title('Welcome to LinkStack!') }
+    it { should have_title('LinkStack') }
     it { should have_content('LinkStack') }
     it { should have_link('Login', href: '#') }
-    it { should have_link('Sign up today!', href: '#') }
+    it { should have_link('Sign up today', href: '#') }
+    it { should have_link('LinkStack', href: root_path) }
+    it { should have_link('About', href: about_path) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_title('LinkStack | About') }
+    it { should have_content('About LinkStack') }
   end
 end
