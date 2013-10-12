d701aa969e85a3efdeb283a35a48921e21a47411
David Becerra <davidbecerra84@gmail.com>
2013-10-12 00:38:00 -0700

    add database cleaner to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,19 @@ Spork.prefork do
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
     config.include Capybara::DSL
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
   end
 end
 
