1dbf08b4046b2a68bba5593278eed164e9a6e983
David Becerra <davidbecerra84@gmail.com>
2014-03-06 20:38:17 -0800

    wip - adding a delete link

--- spec/requests/manage_links_spec.rb
+++ spec/requests/manage_links_spec.rb
@@ -16,7 +16,18 @@ feature 'Manage Links' do
     fill_in('Description', with: 'Description example')
     click_button('Save')
 
+    expect(page).to have_css("#link_#{Link.last.id}")
     expect(page).to have_content('http://google.com')
     expect(page).to have_content('Description example')
+  end
+
+  scenario 'delete a link' do
+    fill_in('URL', with: 'http://google.com')
+    fill_in('Description', with: 'Description example')
+    click_button('Save')
+
+    within("#link_#{Link.last.id}") do
+      click_link('delete')
+    end
   end
 end
